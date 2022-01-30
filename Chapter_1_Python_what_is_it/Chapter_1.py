749d2a3c34d889c32725e4afed97408d5cba48ef
Andrey <alfa_work_mail@mail.ru>
2022-01-30 11:55:24 +0300

    Add Chapter 1,2,3

---
+++ Chapter_1_Python_what_is_it/Chapter_1.py
@@ -0,0 +1,11 @@
+#1
+# Python installed
+
+#2
+# Interpreter started
+
+#3
+print(8 * 9)
+
+#4-5
+print(47)
---
+++ Chapter_2_Data_types_values_variables_and_names/Chapter_2.py
@@ -0,0 +1,18 @@
+#1
+prince = 99
+print(prince)
+
+
+#2
+""" 5 is int"""
+print(type(5))
+
+
+#3
+""" 2.0 is float"""
+print(type(2.0))
+
+
+#4
+""" 5 + 2.0 is float"""
+print(type(5 + 2.0))
