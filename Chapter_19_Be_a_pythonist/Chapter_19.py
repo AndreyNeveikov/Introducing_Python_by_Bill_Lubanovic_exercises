a6414fa7f39d85c764e0b87ac5ff1c29cabaa088
Andrey <alfa_work_mail@mail.ru>
2022-02-23 13:10:33 +0300

    Add list of useful sources (chapter 19)
    There was no exercises after chapter

---
+++ Chapter_19_Be_a_pythonist/Chapter_19.py
@@ -0,0 +1,31 @@
+# There are no exercises after chapter
+# This file would be list of useful sources
+
+
+# Integrated development tools:
+# ___________________________________
+# Jupyter Notebook
+# Jupyter Labs
+
+
+# Code testing
+# ___________________________________
+# pylint, pyflakes, flake8, PEP-8
+# unittest
+# doctest
+# nose
+
+
+# Code debugging
+# ___________________________________
+# print()
+# pdb
+# breakpoint()
+
+
+# Code optimization
+# ___________________________________
+# Cython, C-extensions
+# NumPy
+# PyPy
+# Numba
